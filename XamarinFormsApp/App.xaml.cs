2842a1c3a532d8f3e24feab07f1a4afa30e30f0e
Traian Mihali <traianmihali696@gmail.com>
2020-08-29 23:19:49 +0300

    Add ImagePage class

--- XamarinFormsApp/App.xaml.cs
+++ XamarinFormsApp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinFormsApp.Pages;
 
 namespace XamarinFormsApp
 {
@@ -10,7 +11,7 @@ namespace XamarinFormsApp
         {
             InitializeComponent();
 
-            MainPage = new PayCreditPage1();
+            MainPage = new ImagePage();
         }
 
         protected override void OnStart()
---
+++ XamarinFormsApp/Pages/ImagePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImagePage : ContentPage
+    {
+        public ImagePage()
+        {
+            InitializeComponent();
+
+            SetBackgroundImage();
+        }
+
+        private void SetBackgroundImage()
+        {
+            //var imageSrc = UriImageSource.FromUri(new Uri("http://lorempixel.com/1920/1080/sports/5/"));
+
+            var imageSrc = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/sports/5/") };
+
+            imageSrc.CacheValidity = TimeSpan.FromHours(1);
+            imageSrc.CachingEnabled = false;
+
+            //image.Aspect = Aspect.AspectFill;
+            image.Source = imageSrc;
+
+        }
+    }
+}
