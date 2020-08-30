545bf77c48f5eb779a101be113444c378ce87040
Traian Mihali <traianmihali696@gmail.com>
2020-08-30 10:50:00 +0300

    Add PhotoGallery page

--- XamarinFormsApp/App.xaml.cs
+++ XamarinFormsApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamarinFormsApp
         {
             InitializeComponent();
 
-            MainPage = new ImageCirclePage();
+            MainPage = new PhotoGalleryPage();
         }
 
         protected override void OnStart()
---
+++ XamarinFormsApp/Pages/PhotoGalleryPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhotoGalleryPage : ContentPage
+    {
+        private int _currentImageId = 1;
+
+        public PhotoGalleryPage()
+        {
+            InitializeComponent();
+
+            LoadImage();
+        }
+
+        private void LoadImage()
+        {
+            image.Source = new UriImageSource
+            {
+                Uri = new Uri(String.Format("http://lorempixel.com/1920/1080/city/{0}", _currentImageId)),
+                CachingEnabled = false
+            };
+        }
+
+        private void Previous_Clicked(object sender, EventArgs e)
+        {
+            _currentImageId--;
+            if (_currentImageId < 1)
+                _currentImageId = 10;
+
+            LoadImage();
+
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+            _currentImageId++;
+            if (_currentImageId > 10)
+                _currentImageId = 1;
+
+            LoadImage();
+        }
+    }
+}
