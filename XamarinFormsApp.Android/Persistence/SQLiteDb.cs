f829de0fccb5c74fa5b925efb97736bd677320dc
Traian Mihali <traianmihali696@gmail.com>
2020-09-08 23:06:24 +0300

    Setup SQLite

---
+++ XamarinFormsApp.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using XamarinFormsApp.Persistence;
+
+namespace XamarinFormsApp.Droid.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+
+        }
+    }
+}
---
+++ XamarinFormsApp/Persistence/ISQLiteDb.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsApp.Persistence
+{
+    public interface ISQLiteDb
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
