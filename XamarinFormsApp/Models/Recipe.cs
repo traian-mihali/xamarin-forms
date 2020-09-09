bf6c7dd2910ee1f1071b2837cf5d7c2a260a30b5
Traian Mihali <traianmihali696@gmail.com>
2020-09-09 21:43:27 +0300

    Implement INotifyPropertyChanged interface

--- XamarinFormsApp/Models/Recipe.cs
+++ XamarinFormsApp/Models/Recipe.cs
@@ -1,17 +1,39 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace XamarinFormsApp.Models
 {
     [Table("Recipes")]
-    public class Recipe
+    public class Recipe : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _name;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
         [MaxLength(255)]
-        public string Name { get; set; }
+        public string Name { 
+            get { return _name; }
+            set
+            {
+                if (_name == value)
+                    return;
+
+                _name = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
